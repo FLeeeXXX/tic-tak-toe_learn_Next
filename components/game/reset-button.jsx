8bc3f75c76d03f972d8824a3fba0be729f3c5837
Danil Shulgin <43463155+FLeeeXXX@users.noreply.github.com>
2024-06-29 03:13:03 +0500

    realization of the layout in process

--- components/game/reset-button.jsx
+++
@@ -1,6 +0,0 @@
-import {React} from "react";
-
-export function ResetButton({ handleClearCell }) {
-    return <button className='cursor-pointer mt-2.5 bg-transparent border border-gray-400 py-1 px-3 rounded'
-                   onClick={handleClearCell}>Сбросить</button>
-}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,13 @@
 import {React} from 'react';
 import { Header } from '../components/header';
+import {GameInfo, GameTitle} from "../components/game";
 
 export default function HomePage() {
     return (<div className="bg-slate-50 min-h-screen">
         <Header/>
+        <main className="pt-6 mx-auto max-w-[616px]">
+            <GameTitle/>
+            <GameInfo className="mt-4"/>
+        </main>
     </div>)
 }
